889e016099729c485f5c4a2987f0b4c09d58b628
Hamzat Azeez <azeezhamzy99@gmail.com>
2022-07-31 15:14:22 +0100

    New Changes

--- backend/src/api.py
+++ backend/src/api.py
@@ -4,8 +4,8 @@ from sqlalchemy import exc
 import json
 from flask_cors import CORS
 
-from database.models import db_drop_and_create_all, setup_db, Drink
-from auth.auth import AuthError, requires_auth, get_token_auth_header
+from .database.models import db_drop_and_create_all, setup_db, Drink
+from .auth.auth import AuthError, requires_auth, get_token_auth_header
 
 app = Flask(__name__)
 setup_db(app)
